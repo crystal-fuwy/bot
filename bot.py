c4d2717bbafc2295f8e112bb45324b58cf857901
Crystal Fur <91292470+crystal-fuwy@users.noreply.github.com>
2021-09-27 12:55:48 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,16 @@
-# Import the Discord modules
 import discord
 
-# Define client
 client = discord.Client()
 
-# On connection to Discord event
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Game('cfur help'))
+    await client.change_presence(activity=discord.Game('cfur help | revision 11'))
     print('Online!')
 
-# On the message that triggers the command
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
-        
-    if message.content.startswith('cfur help'):
-        await mssage.channel.send('`Crystal Fur#5411` has big plans for this bot!')
 
-# Discord bot token to identify the application
-client.run('discord bot token')
+    if message.content.startswith('cfur info'):
+      await message.channel.send(embed = discord.Embed(title="Crystal Fur Bot", description='Crystal Fur Bot\nRevision 11\nOwner: Crystal Fur#5411'))
+
+client.run('token')
